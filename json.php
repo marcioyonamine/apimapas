bc7959e6b2d29b5f706279a7c8390fdf3ad7aab4
Marcio Yonamine <marcioyonamine@gmail.com>
2015-08-26 00:21:42 -0300

    primeiras brincadeiras

---
+++ json.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+$url = "http://spcultura.prefeitura.sp.gov.br/api/event/find";
+$data = array("@select" => "id, name, longDescription, createTimestamp", "@order" => "name ASC", "owner" => "EQ(@Agent:608)");
+$get_addr = $url.'?'.http_build_query($data);
+$ch = curl_init($get_addr);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$page = curl_exec($ch);
+echo "<pre>";
+print_r(json_decode($page));
+echo "</pre>";
+?>
