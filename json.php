3197c0b22652171e0e4c7881da41ca613b5cee69
Marcio Yonamine <marcioyonamine@gmail.com>
2015-08-26 10:51:11 -0300

    comparar datas

--- json.php
+++ json.php
@@ -30,18 +30,35 @@ $page = curl_exec($ch);
 $evento = json_decode($page);
 
 
-echo "Foram encontrados ".sizeof($evento)." resultados.";
-/*
-echo "<pre>";
-print_r(json_decode($page));
-echo "</pre>"; 
-*/
+echo "Foram encontrados ".sizeof($evento)." resultados.<br><br>";
 
 $ccsp = converterObjParaArray($evento);
+/*
+echo "<pre>";
+print_r($ccsp);
+echo "</pre>";
+*/
+
+// Retorna os eventos de hoje (converter a string date para date e fazer a comparação de datas)
+$hoje = date('Y-m-d');
+echo "Hoje é $hoje<br>";
+
+for($i = 0; $i < sizeof($evento); $i++){
+	for($k = 0; $k < sizeof($ccsp[$i]['occurrences']); $k++){
+		if($ccsp[0]['occurrences'][0]['rule']['until'] != ""){
+			if($ccsp[0]['occurrences'][0]['rule']['startsOn'] >= $hoje AND $ccsp[0]['occurrences'][0]['rule']['until'] <= $hoje ){
+				echo $ccsp[$i]['name']."<br>";
+			}else if ($ccsp[0]['occurrences'][0]['rule']['startsOn'] == $hoje){
+			}
+		}
+	}
+}
+
+//echo $ccsp[0]['occurrences'][0]['rule']['startsOn']; //Exemplo de acesso a informação
+
 echo "<pre>";
 print_r($ccsp);
 echo "</pre>";
 
-echo $ccsp[0]['occurrences'][0]['rule']['startsAt']; //Exemplo de acesso a informação
 
 ?>
